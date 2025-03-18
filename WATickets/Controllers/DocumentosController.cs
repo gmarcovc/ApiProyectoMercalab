6baf438e9d503b1bfbe93ade66add767f95cfd07
Andres10V <77872022+Andres10V@users.noreply.github.com>
2025-03-17 21:45:04 -0600

    Documentos Rutas

---
+++ WATickets/Controllers/DocumentosController.cs
@@ -0,0 +1,272 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WATickets.Models;
+using WATickets.Models.Cliente;
+
+namespace WATickets.Controllers
+{
+    public class DocumentosController : ApiController
+    {
+        ModelCliente db = new ModelCliente();
+
+        public HttpResponseMessage GetAll([FromUri] Filtros filtro)
+        {
+            try
+            {
+
+                var time = new DateTime(); // 01-01-0001
+                if (filtro.FechaFinal != time)
+                {
+                    filtro.FechaInicial = filtro.FechaInicial.Date;
+                    filtro.FechaFinal = filtro.FechaFinal.AddDays(1);
+                }
+                var Documentos = db.EncDocumento.Select(a => new
+                {
+                    a.id,
+                    a.idCliente,
+                    a.Fecha,
+                    a.Comentarios,
+                    a.Subtotal,
+                    a.TotalImpuestos,
+                    a.TotalDescuentos,
+                    a.TotalCompra,
+                    a.PorDescto,
+                    a.Moneda,
+                    a.idCondPago,
+                    a.idRuta,
+
+                    Detalle = db.DetDocumento.Where(b => b.idEncabezado == a.id).ToList()
+
+                }).Where(a => (filtro.FechaInicial != time ? a.Fecha >= filtro.FechaInicial : true) && (filtro.FechaFinal != time ? a.Fecha <= filtro.FechaFinal : true)).ToList(); //Traemos el listado de productos
+
+
+                if (filtro.Codigo1 > 0) // esto por ser integer
+                {
+                    Documentos = Documentos.Where(a => a.idCliente == filtro.Codigo1).ToList(); // filtramos por lo que traiga el codigo1 
+                }
+
+
+
+
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK, Documentos);
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StrackTrace = ex.StackTrace;
+                be.Fecha = DateTime.Now;
+                be.JSON = JsonConvert.SerializeObject(ex);
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex);
+
+            }
+
+
+        }
+        [Route("api/Documentos/Consultar")]
+        public HttpResponseMessage GetOne([FromUri] int id)
+        {
+            try
+            {
+                var Documento = db.EncDocumento.Select(a => new
+                {
+                    a.id,
+                    a.idCliente,
+                    a.Fecha,
+                    a.Comentarios,
+                    a.Subtotal,
+                    a.TotalImpuestos,
+                    a.TotalDescuentos,
+                    a.TotalCompra,
+                    a.PorDescto,
+                    a.Moneda,
+                    a.idCondPago,
+                    a.idRuta,
+
+                    Detalle = db.DetDocumento.Where(b => b.idEncabezado == a.id).ToList()
+
+                }).Where(a => a.id == id).FirstOrDefault();
+
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK, Documento);
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StrackTrace = ex.StackTrace;
+                be.Fecha = DateTime.Now;
+                be.JSON = JsonConvert.SerializeObject(ex);
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex);
+
+            }
+
+        }
+
+        [Route("api/Documentos/Insertar")]
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] Documentos documento)
+        {
+            try
+            {
+                EncDocumento Documento = db.EncDocumento.Where(a => a.id == documento.id).FirstOrDefault();
+                if (Documento == null)
+                {
+                    Documento = new EncDocumento();
+                    Documento.idCliente = documento.idCliente;
+                    Documento.idCondPago = documento.idCondPago;
+                    Documento.idRuta = documento.idRuta;
+                    Documento.Fecha = DateTime.Now;
+                    Documento.Comentarios = documento.Comentarios;
+                    Documento.Subtotal = documento.Subtotal;
+                    Documento.TotalImpuestos = documento.TotalImpuestos;
+                    Documento.TotalDescuentos = documento.TotalDescuentos;
+                    Documento.TotalCompra = documento.TotalCompra;
+                    Documento.PorDescto = documento.PorDescto;
+                    Documento.Moneda = documento.Moneda;
+                    Documento.BaseEntry = documento.BaseEntry;
+                    db.EncDocumento.Add(Documento);
+                    db.SaveChanges();
+
+                    var i = 0;
+                    foreach (var item in documento.Detalle)
+                    {
+                        DetDocumento det = new DetDocumento();
+                        det.idEncabezado = Documento.id;
+                        det.idProducto = item.idProducto;
+                        det.NumLinea = i;
+                        det.PorDescto = item.PorDescto;
+                        det.PrecioUnitario = item.PrecioUnitario;
+                        det.TotalImpuesto = item.TotalImpuesto;
+                        det.Cantidad = item.Cantidad;
+                        det.Descuento = item.Descuento;
+                        det.TotalLinea = item.TotalLinea;
+
+                        db.DetDocumento.Add(det);
+                        db.SaveChanges();
+                        i++;
+
+                        var prod = db.Productos.Where(a => a.id == item.idProducto).FirstOrDefault();
+                        if (prod != null)
+                        {
+
+                            db.Entry(prod).State = EntityState.Modified;
+                            prod.Stock -= item.Cantidad;
+                            db.SaveChanges();
+                        }
+                    }
+                }
+                else
+                {
+                    throw new Exception("Ya existe una documento con este ID");
+                }
+                documento.id = Documento.id;
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK, documento);
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StrackTrace = ex.StackTrace;
+                be.Fecha = DateTime.Now;
+                be.JSON = JsonConvert.SerializeObject(ex);
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex);
+            }
+        }
+        [Route("api/Documentos/Actualizar")]
+        [HttpPut]
+        public HttpResponseMessage Put([FromBody] Documentos documento)
+        {
+            var t = db.Database.BeginTransaction();
+            try
+            {
+                EncDocumento Documento = db.EncDocumento.Where(a => a.id == documento.id).FirstOrDefault();
+                if (Documento != null)
+                {
+                    db.Entry(Documento).State = EntityState.Modified;
+                    Documento.idCliente = documento.idCliente;
+                    Documento.idCondPago = documento.idCondPago;
+                    Documento.idRuta = documento.idRuta;
+                    Documento.Fecha = DateTime.Now;
+                    Documento.Comentarios = documento.Comentarios;
+                    Documento.Subtotal = documento.Subtotal;
+                    Documento.TotalImpuestos = documento.TotalImpuestos;
+                    Documento.TotalDescuentos = documento.TotalDescuentos;
+                    Documento.TotalCompra = documento.TotalCompra;
+                    Documento.PorDescto = documento.PorDescto;
+                    Documento.Moneda = documento.Moneda;
+
+
+                    db.SaveChanges();
+
+                    var Detalles = db.DetDocumento.Where(a => a.idEncabezado == Documento.id).ToList();
+
+                    foreach (var item in Detalles)
+                    {
+                        db.DetDocumento.Remove(item);
+                        db.SaveChanges();
+                    }
+
+
+                    var i = 0;
+                    foreach (var item in documento.Detalle)
+                    {
+                        DetDocumento det = new DetDocumento();
+                        det.idEncabezado = Documento.id;
+                        det.idProducto = item.idProducto;
+                        det.NumLinea = i;
+                        det.PorDescto = item.PorDescto;
+                        det.PrecioUnitario = item.PrecioUnitario;
+                        det.TotalImpuesto = item.TotalImpuesto;
+                        det.Cantidad = item.Cantidad;
+                        det.Descuento = item.Descuento;
+                        det.TotalLinea = item.TotalLinea;
+
+                        db.DetDocumento.Add(det);
+                        db.SaveChanges();
+                        i++;
+                    }
+
+
+
+                    t.Commit();
+                }
+                else
+                {
+                    throw new Exception("NO existe una documento con este ID");
+                }
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                t.Rollback();
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StrackTrace = ex.StackTrace;
+                be.Fecha = DateTime.Now;
+                be.JSON = JsonConvert.SerializeObject(ex);
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, be);
+            }
+        }
+    }
+}
---
+++ WATickets/Controllers/RutasController.cs
@@ -0,0 +1,179 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WATickets.Models;
+using WATickets.Models.Cliente;
+
+
+namespace WATickets.Controllers
+{
+    [Authorize]
+    public class RutasController : ApiController
+    {
+        ModelCliente db = new ModelCliente();
+
+        public HttpResponseMessage GetAll([FromUri] Filtros filtro)
+        {
+            try
+            {
+                var Rutas = db.Rutas.ToList();
+
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK, Rutas);
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StrackTrace = ex.StackTrace;
+                be.Fecha = DateTime.Now;
+                be.JSON = JsonConvert.SerializeObject(ex);
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex);
+
+            }
+
+        }
+        [Route("api/Rutas/Consultar")]
+        public HttpResponseMessage GetOne([FromUri] int id)
+        {
+            try
+            {
+                Rutas rutas = db.Rutas.Where(a => a.id == id).FirstOrDefault();
+
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK, rutas);
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StrackTrace = ex.StackTrace;
+                be.Fecha = DateTime.Now;
+                be.JSON = JsonConvert.SerializeObject(ex);
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex);
+
+            }
+
+        }
+        [Route("api/Rutas/Insertar")]
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] Rutas rutas)
+        {
+            try
+            {
+                Rutas Ruta = db.Rutas.Where(a => a.id == rutas.id).FirstOrDefault();
+                if (Ruta == null)
+                {
+                    Ruta = new Rutas();
+                    Ruta.Nombre = rutas.Nombre;
+                    Ruta.Origen = rutas.Origen;
+                    Ruta.Costos = rutas.Costos;
+                    Ruta.Km = rutas.Km;
+                    db.Rutas.Add(Ruta);
+                    db.SaveChanges();
+
+                }
+                else
+                {
+                    throw new Exception("Ya existe una ruta con este ID");
+                }
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StrackTrace = ex.StackTrace;
+                be.Fecha = DateTime.Now;
+                be.JSON = JsonConvert.SerializeObject(ex);
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex);
+            }
+        }
+        [Route("api/Rutas/Actualizar")]
+        [HttpPut]
+        public HttpResponseMessage Put([FromBody] Rutas rutas)
+        {
+            try
+            {
+                Rutas Ruta = db.Rutas.Where(a => a.id == rutas.id).FirstOrDefault();
+                if (Ruta != null)
+                {
+                    db.Entry(Ruta).State = System.Data.Entity.EntityState.Modified;
+                    Ruta.Nombre = rutas.Nombre;
+                    Ruta.Origen = rutas.Origen;
+                    Ruta.Costos = rutas.Costos;
+                    Ruta.Km = rutas.Km;
+                    db.SaveChanges();
+
+                }
+                else
+                {
+                    throw new Exception("No existe una ruta" +
+                        " con este ID");
+                }
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StrackTrace = ex.StackTrace;
+                be.Fecha = DateTime.Now;
+                be.JSON = JsonConvert.SerializeObject(ex);
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex);
+            }
+        }
+        [Route("api/Rutas/Eliminar")]
+        [HttpDelete]
+        public HttpResponseMessage Delete([FromUri] int id)
+        {
+            try
+            {
+                Rutas Rutas = db.Rutas.Where(a => a.id == id).FirstOrDefault();
+                if (Rutas != null)
+                {
+                    db.Rutas.Remove(Rutas);
+                    db.SaveChanges();
+
+                }
+                else
+                {
+                    throw new Exception("No existe una bodega con este ID");
+                }
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StrackTrace = ex.StackTrace;
+                be.Fecha = DateTime.Now;
+                be.JSON = JsonConvert.SerializeObject(ex);
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex);
+            }
+        }
+    }
+}
